75646e6c624a3d1ec33fef30bd92667e8fdf1a21
ygoralvess <ygornalves@gmail.com>
2023-10-06 17:22:13 -0300

    Bom dms ta

--- Javascript 2/assets/js/script.js
+++ Javascript 2/assets/js/script.js
@@ -2,9 +2,10 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    fetch(url).then(function(response)  {
-    response.json().then(function(responseBody) {
-        return response.json()
+
+fetch(url)
+    .then(function(response)  {
+         return response.json()
     })
     .then(function(jsonBody) {
         response
@@ -12,10 +13,8 @@ const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
         .then(function (responseBody) {
             console.log(responseBody)
         })
-        }
     })
-    })
-    .catch(function(error) {
+        .catch(function(error) {
         console.error(error)
     })
     .finally(function() {
