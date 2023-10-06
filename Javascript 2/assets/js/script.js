d12b2c573b8e0eefc0482a5da3c47d6eee73e147
ygoralvess <ygornalves@gmail.com>
2023-10-06 18:23:35 -0300

    Finalmente

--- Javascript 2/assets/js/script.js
+++ Javascript 2/assets/js/script.js
@@ -2,8 +2,37 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function convertPokemonToLi(pokemon) {
+    return`
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+
+        <div class="detail">
+            <ol class="types">
+             <li class="type">grass</li>
+             <li class="type">poison</li>
+
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPi/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+        alt=${pokemon.name}>
+ 
+     </div>
+    </li>
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
 
 fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        for (let index = 0; index < pokemons.length; index++) {
+            const pokemon = pokemons[index];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        }
+    })
+
     .catch((error) => console.error(error))
