cd5e881327f8fe25ba704b50e2e371f7a59ffd51
ygoralvess <ygornalves@gmail.com>
2023-10-11 00:00:15 -0300

    ==+

--- Javascript 2/assets/js/pokeapi.js
+++ Javascript 2/assets/js/pokeapi.js
@@ -3,15 +3,17 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map((pokemon) => fetch(pokemon.url).then((response) => response.json())))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonDetails) => {
-        debugger
-        console.log(pokemonDetails)
-    })
+    .then((pokemonsDetails) => pokemonsDetails)
 }
